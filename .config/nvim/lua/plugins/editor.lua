5b924508844aefc79e8a31d655edab996be078a9
David Vílchez <dvilchez@xuaps.com>
2024-03-07 19:08:44 +0100

    Add reveal

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,6 +1,19 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
+    keys = {
+      {
+        "<leader>fe",
+        function()
+          require("neo-tree.command").execute({
+            toggle = true,
+            dir = require("lazy.core.config").options.root,
+            reveal = true,
+          })
+        end,
+        desc = "Explorer NeoTree (root dir)",
+      },
+    },
     opts = {
       window = {
         position = "current",
