4f4a44f71f0761d7d4fb1b7880ad6f8b2b939869
David Vílchez <dvilchez@xuaps.com>
2024-03-08 10:06:57 +0100

    Disable some plugins

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,6 @@
 return {
   { "rcarriga/nvim-notify", enabled = false },
+  { "folke/noice.nvim", enabled = false },
   {
     "folke/flash.nvim",
     enabled = false,
--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -17,9 +17,19 @@ return {
       },
     },
     opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+          hide_dotfiles = false,
+          hide_gitignored = true,
+        },
+      },
       window = {
         position = "current",
       },
     },
+  },
+  {
+    "almo7aya/openingh.nvim",
   },
 }
